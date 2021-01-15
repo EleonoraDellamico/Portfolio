3a7bc9d3b7c500cf976e8c6a3ca0c86e3cc957f5
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-01-15 20:35:52 +0100

    title contact

--- index.js
+++ index.js
@@ -6,32 +6,3 @@ function myFunction () {
    let element = document.querySelector(".menuppal");
    element.classList.toggle("hidden");
 }
-
- let elements = document.querySelector(".column");
-
- let i;
-
-// Full-width images
-function one() {
-    for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "100%";  
-    elements[i].style.flex = "100%";
-  }
-}
-
-// Two images side by side
-function two() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "50%";  // IE10
-    elements[i].style.flex = "50%";
-  }
-}
-
-// Four images side by side
-function four() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
-  }
-}
-
