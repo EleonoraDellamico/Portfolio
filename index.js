6526b070ee42e95b69350b9eb5aff1b462d4380e
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-01-25 17:19:49 +0100

    added link proyect

--- index.js
+++ index.js
@@ -5,56 +5,56 @@ const menu = [
     title: "Landing page",
     category: "htmlCss",
     img: "./img/NY.jpg",
-    desc: `bla bal bla bla bla bla bla `
+    a: `http://beta.adalab.es/modulo-1-evaluacion-final-EleonoraDellamico/ `
   },
   {
     id: 2,
     title: "Cinema",
     category: "htmlCss",
     img: "./img/Cinema.jpg",
-    desc: `bla bal bla bla bla bla bla `
+    
   },
   {
     id: 3,
-    title: "Marea Coders Web page Html&Css",
+    title: "Marea Coders Web Html&Css",
     category: "htmlCss",
     img: "./img/Sea.jpg",
-    desc: `bla bal bla bla bla bla bla `
+    a: `https://github.com/martreyz/project-promo-k-module-1-team-7 `
   },
   {
     id: 4,
     title: "Memory Card JavaScript",
     category: "javascript",
     img: "./img/MemoryCard.jpg",
-   desc: `bla bal bla bla bla bla bla `
+    a: "https://eleonoradellamico.github.io/LearnJavaScrip_MemoryGame/"
   },
   {
     id: 5,
     title: "Pokemon React",
     category: "react",
     img: "./img/Pokemon.jpg",
-   desc: `bla bal bla bla bla bla bla `
+   a: `http://beta.adalab.es/modulo-3-evaluacion-intermedia-EleonoraDellamico/`
   },
   {
     id: 6,
     title: " Visiting Card JavaScript",
     category: "javascript",
     img: "./img/Superheros.jpg",
-    desc: `bla bal bla bla bla bla bla `
+    a: "https://github.com/martreyz/project-promo-k-module-3-team-4" 
   },
   {
     id: 7,
     title: "Rick&Morty React",
     category: "react",
     img: "./img/Rick-Morty.jpg",
-    desc: "bla bla bla bla bla bla "
+    a: "http://beta.adalab.es/modulo-3-evaluacion-final-EleonoraDellamico/#/"
   },
   {
     id: 8,
     title: "from JavaScript to React",
     category: "react",
     img: "./img/ReactCard.jpg",
-    desc: "bla bla bla bla bla"  
+    a: "http://beta.adalab.es/project-promo-K-module-02-team-05/"  
   }, 
 ];
 console.log("eli");
@@ -85,8 +85,8 @@ alt=${item.title} />
 		<header>
 			<h4>${item.title}</h4>
     </header>
-    <p class="item-text">${item.desc} </
-p>
+    
+<a href="${item.a}" target="_blank"> See the project</a>
 	</div>
 </article>`;
   });
